bedeeef8bc37ce7076657ec8653e22fc3b12c973
dashaa <cybordashaa@gmail.com>
2020-04-20 11:52:04 +0800

    add(): db

---
+++ src/database/database.service.ts
@@ -0,0 +1,26 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigServise } from 'src/config/config.service';
+import { ConnectionOptions } from 'typeorm';
+import { Configuration } from 'src/config/config.keys';
+
+
+export const databaseProviders = [
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigServise],
+        async useFactory(config: ConfigServise) {
+            return {
+                ssl: false,
+                type: 'mysql' as 'mysql',
+                host: config.get(Configuration.HOST),
+                username: config.get(Configuration.USERNAME),
+                password: config.get(Configuration.PASSWORD),
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                migrations: [__dirname + '/migrations/*{.ts,.js}']
+
+            } as ConnectionOptions
+
+        }
+    })
+];
