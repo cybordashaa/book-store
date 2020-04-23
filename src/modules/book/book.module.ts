b0c68997f86f863f4edbfb1374f1799da3dc3d4d
dashaa <cybordashaa@gmail.com>
2020-04-23 14:43:05 +0800

    fix(): authmodule import in bookmodule

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -4,12 +4,14 @@ import { BookService } from './book.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookRepository } from './book.repository';
 import { UserRepository } from '../user/user.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       BookRepository, UserRepository
-    ])
+    ]),
+    AuthModule
   ],
   controllers: [BookController],
   providers: [BookService]
